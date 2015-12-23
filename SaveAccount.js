70469a2d9d798d8c8b129dc6651acf47e291750f
Yuyi Lin <linyiyu1992@gmail.com>
2015-12-24 02:26:33 +0800

    登录done

--- SaveAccount.js
+++ SaveAccount.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react-native');
+var md5 = require('md5');
 
 var {
     View,
@@ -16,6 +17,8 @@ var {
     TouchableHighlight,
     AsyncStorage,
 } = React;
+
+var BODY_CONST = "_json=true&callback=https%3A%2F%2Faccount.xiaomi.com%2Fsts%3Fsign%3DZvAtJIzsDsFe60LdaPa76nNNP58%253D%26followup%3Dhttps%253A%252F%252Faccount.xiaomi.com%252Fpass%252Fauth%252Fsecurity%252Fhome%26sid%3Dpassport&sid=passport&qs=%253Fcallback%253Dhttps%25253A%25252F%25252Faccount.xiaomi.com%25252Fsts%25253Fsign%25253DZvAtJIzsDsFe60LdaPa76nNNP58%2525253D%252526followup%25253Dhttps%2525253A%2525252F%2525252Faccount.xiaomi.com%2525252Fpass%2525252Fauth%2525252Fsecurity%2525252Fhome%252526sid%25253Dpassport%2526sid%253Dpassport&_sign=2%26V1_passport%26JO7oplyppgkN%2FTiDH69nleQr10g%3D&serviceParam=%7B%22checkSafePhone%22%3Afalse%7D&";
 
 var SaveAccount = React.createClass({
 
@@ -44,6 +47,23 @@ var SaveAccount = React.createClass({
     ).done();
 
     // TODO 验证账号
+    fetch('https://account.xiaomi.com/pass/serviceLoginAuth2', {
+      method: 'POST',
+      headers: {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      },
+      body: BODY_CONST+'user='+ encodeURIComponent(this.state.account) + "&hash=" + md5(this.state.passwd)
+    })
+    .then((response) => console.log( response.headers ))
+    // .then((response) => response.headers.json())
+    // .then((responseData) => {
+    //     AlertIOS.alert(
+    //         "POST Response",
+    //         "Response Body -> " + JSON.stringify(responseData.body)
+    //     )
+    // })
+    .done();
 
 
     // console.log(this.props.navigator);
